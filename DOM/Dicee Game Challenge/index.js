907576eecc90381d47bb8e22b9a58126126d7a9c
Stephen de Goede <stephendegoede@users.noreply.github.com>
2020-05-19 15:05:51 +0200

    Completed the dicee challenge correctly

---
+++ DOM/Dicee Game Challenge/index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+document.querySelector(".img1").setAttribute("src", "images/dice"+ randomNumber1 + ".png");
+
+document.querySelector(".img2").setAttribute("src", "images/dice"+ randomNumber2 + ".png");
+
+if(randomNumber1 == randomNumber2){
+  document.querySelector("h1").textContent = "Draw!";
+}
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").textContent = "Player 1 Wins!";
+}
+
+if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").textContent = "Player 2 Wins!";
+}
