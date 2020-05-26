6bf4e94f23ec0d1374719ede29a669281b2c6eb9
Stephen de Goede <stephendegoede@users.noreply.github.com>
2020-05-26 11:56:47 +0200

    Learned about Nodejs, purpose, usage, how to run JS using node

---
+++ Nodejs&Expressjs/intro-to-node/index.js
@@ -0,0 +1,7 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var superhero = superheroes.random();
+var supervillain = supervillains.random();
+
+console.log(superhero + " beat " + supervillain);
