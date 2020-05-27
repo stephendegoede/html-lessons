b07d494887d498ff068963902ceaa6f2bc86a7a5
Stephen de Goede <stephendegoede@users.noreply.github.com>
2020-05-27 11:38:47 +0200

    Added bodyparser to use form data req and perform calc on inputs

--- Nodejs&Expressjs/Calculator/calculator.js
+++ Nodejs&Expressjs/Calculator/calculator.js
@@ -1,10 +1,25 @@
 //jshint esversion:6
 
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Receive requests and send responses
 app.get ("/", function (req, res){
     res.sendFile(__dirname + "/index.html");
+});
+
+//respond to the "post" request sent by the form
+app.post("/", function (req, res){
+  var1 = Number(req.body.num1);
+  var2 = Number(req.body.num2);
+
+  result = var1 + var2;
+
+  res.send("The result of the calculation is " + result);
+
 });
 
 app.listen (3000, function(){
